abd11253b747a222461e4bcc7842edd4136ab4a2
BLOUplanet <147328072+llaa33219@users.noreply.github.com>
2025-09-29 19:18:37 +0900

    Update hummm.js

--- hummm.js
+++ hummm.js
@@ -1,2 +1,6 @@
+// 이거로 확인하고
 Entry.options.useWebGL ? console.log("WebGL 모드") : console.log("Canvas 모드");
+// 이거로 webgl 활성화
 Entry.options.useWebGL = true;
+// 이거로 canvas 활성화(webgl 비활성화)
+Entry.options.useWebGL = false;
